8d91b6dc681469e52e672ac37dbac3d8a808f45c
Maeva Masson <maeva.masson@gmail.com>
2019-08-16 20:19:48 +0200

    ajout de la fonction pour la page description produit f

--- js/script2_f.js
+++ js/script2_f.js
@@ -1,4 +1,5 @@
-//-------------Produits Femme-----------------
+
+//-------------Page tous les Produits Femme-----------------
 var time = 3000;
 
 var pF = 0;
@@ -8,10 +9,10 @@ PanF[1] = 'img/femme/PANTALONS/pantalon3.jpg';
 PanF[2] = 'img/femme/PANTALONS/pantalon4.jpg';
 
 var jF = 0;
-var jeansF = [];
-jeansF[0] = 'img/femme/JEANS/jean2.jpg';
-jeansF[1] = 'img/femme/JEANS/jean3.jpg';
-jeansF[2] = 'img/femme/JEANS/jean4.jpg';
+var jeans_F = [];
+jeans_F[0] = 'img/femme/JEANS/jean2.jpg';
+jeans_F[1] = 'img/femme/JEANS/jean3.jpg';
+jeans_F[2] = 'img/femme/JEANS/jean4.jpg';
 
 var cF = 0;
 var CheF = [];
@@ -20,46 +21,46 @@ CheF[1] = 'img/femme/CHEMISIERS/chemisier3.jpg';
 CheF[2] = 'img/femme/CHEMISIERS/chemisier4.jpg';
 
 var topF = 0;
-var toptopsF = [];
-toptopsF[0] = 'img/femme/HAUTS/haut2.jpg';
-toptopsF[1] = 'img/femme/HAUTS/haut3.jpg';
-toptopsF[2] = 'img/femme/HAUTS/haut4.jpg';
+var tops_F = [];
+tops_F[0] = 'img/femme/HAUTS/haut2.jpg';
+tops_F[1] = 'img/femme/HAUTS/haut3.jpg';
+tops_F[2] = 'img/femme/HAUTS/haut4.jpg';
 
-var tsF = 0;
-var teeF = [];
-teeF[0] = 'img/femme/HAUTS/haut2.jpg';
-teeF[1] = 'img/femme/HAUTS/haut3.jpg';
-teeF[2] = 'img/femme/HAUTS/haut4.jpg';
+var rb = 0;
+var rob = [];
+rob[0] = 'img/femme/ROBES/robe1.jpg';
+rob[1] = 'img/femme/ROBES/robe2.jpg';
+rob[2] = 'img/femme/ROBES/robe3.jpg';
 
 var PuF = 0;
-var pullF = [];
-pullF[0] = 'img/femme/PULLS/pull2.jpg';
-pullF[1] = 'img/femme/PULLS/pull3.jpg';
-pullF[2] = 'img/femme/PULLS/pull4.jpg';
+var pull_F = [];
+pull_F[0] = 'img/femme/PULLS/pull2.jpg';
+pull_F[1] = 'img/femme/PULLS/pull3.jpg';
+pull_F[2] = 'img/femme/PULLS/pull4.jpg';
 
 var vF = 0;
-var vesteF = [];
-vesteF[0] = 'img/femme/VESTES/veste2.jpg';
-vesteF[1] = 'img/femme/VESTES/veste3.jpg';
-vesteF[2] = 'img/femme/VESTES/veste4.jpg';
+var veste_F = [];
+veste_F[0] = 'img/femme/VESTES/veste2.jpg';
+veste_F[1] = 'img/femme/VESTES/veste3.jpg';
+veste_F[2] = 'img/femme/VESTES/veste4.jpg';
 
 var chF = 0;
-var chaussuresF = [];
-chaussuresF[0] = 'img/femme/CHAUSSURES/chaussures2.jpg';
-chaussuresF[1] = 'img/femme/CHAUSSURES/chaussures3.jpg';
-chaussuresF[2] = 'img/femme/CHAUSSURES/chaussures4.jpg';
+var chaussures_F = [];
+chaussures_F[0] = 'img/femme/CHAUSSURES/chaussures2.jpg';
+chaussures_F[1] = 'img/femme/CHAUSSURES/chaussures3.jpg';
+chaussures_F[2] = 'img/femme/CHAUSSURES/chaussures4.jpg';
 
 var manF = 0;
-var manteauxF = [];
-manteauxF[0] = 'img/femme/MANTEAUX/manteau2.jpg';
-manteauxF[1] = 'img/femme/MANTEAUX/manteau3.jpg';
-manteauxF[2] = 'img/femme/MANTEAUX/manteau1.jpg';
+var manteaux_F = [];
+manteaux_F[0] = 'img/femme/MANTEAUX/manteau2.jpg';
+manteaux_F[1] = 'img/femme/MANTEAUX/manteau3.jpg';
+manteaux_F[2] = 'img/femme/MANTEAUX/manteau1.jpg';
 
 var aF = 0;
-var accessF = [];
-accessF[0] = 'img/femme/ACCESSOIRES/ceinture2.jpg';
-accessF[1] = 'img/femme/ACCESSOIRES/montre2.jpg';
-accessF[2] = 'img/femme/ACCESSOIRES/ceinture1.jpg';
+var accessoiresF = [];
+accessoiresF[0] = 'img/femme/ACCESSOIRES/ceinture2.jpg';
+accessoiresF[1] = 'img/femme/ACCESSOIRES/montre2.jpg';
+accessoiresF[2] = 'img/femme/ACCESSOIRES/ceinture1.jpg';
 
 function changeImg11() {
     document.pantalonsF.src = PanF[pF];
@@ -73,8 +74,8 @@ function changeImg11() {
 }
 
 function changeImg12() {
-    document.jeansF.src = jeansF[jF];
-    if (jF < jeansF.length - 1) {
+    document.jeansF.src = jeans_F[jF];
+    if (jF < jeans_F.length - 1) {
         jF++
     } else {
         jF = 0
@@ -95,8 +96,8 @@ function changeImg13() {
 }
 
 function changeImg14() {
-    document.topsF.src = toptopsF[topF];
-    if (topF < toptopsF.length - 1) {
+    document.topsF.src = tops_F[topF];
+    if (topF < tops_F.length - 1) {
         topF++
     } else {
         topF = 0
@@ -106,19 +107,19 @@ function changeImg14() {
 }
 
 function changeImg15() {
-    document.tshirtF.src = teeF[tsF];
-    if (tsF < teeF.length - 1) {
-        tsF++
+    document.robes.src = rob[rb];
+    if (rb < rob.length - 1) {
+        rb++
     } else {
-        tsF = 0
+        rb = 0
     }
 
     setTimeout("changeImg15()", time);
 }
 
 function changeImg16() {
-    document.pullF.src = pullF[PuF];
-    if (PuF < pullF.length - 1) {
+    document.pullF.src = pull_F[PuF];
+    if (PuF < pull_F.length - 1) {
         PuF++
     } else {
         PuF = 0
@@ -128,8 +129,8 @@ function changeImg16() {
 }
 
 function changeImg17() {
-    document.vesteF.src = vesteF[vF];
-    if (vF < vesteF.length - 1) {
+    document.vesteF.src = veste_F[vF];
+    if (vF < veste_F.length - 1) {
         vF++
     } else {
         vF = 0
@@ -139,30 +140,30 @@ function changeImg17() {
 }
 
 function changeImg18() {
-    document.chaussuresF.src = chaussuresF[chF];
-    if (chF < chaussuresF.length - 1) {
-        chF++
+    document.manteauxF.src = manteaux_F[manF];
+    if (manF < manteaux_F.length - 1) {
+        manF++
     } else {
-        chF = 0
+        manF = 0
     }
 
     setTimeout("changeImg18()", time);
 }
 
 function changeImg19() {
-    document.manteauxF.src = manteauxF[manF];
-    if (manF < manteauxF.length - 1) {
-        manF++
+    document.chaussuresF.src = chaussures_F[chF];
+    if (chF < chaussures_F.length - 1) {
+        chF++
     } else {
-        manF = 0
+        chF = 0
     }
 
     setTimeout("changeImg19()", time);
 }
 
 function changeImg20() {
-    document.accessF.src = accessF[aF];
-    if (aF < accessF.length - 1) {
+    document.accessF.src = accessoiresF[aF];
+    if (aF < accessoiresF.length - 1) {
         aF++
     } else {
         aF = 0
@@ -174,3 +175,23 @@ function changeImg20() {
 window.onload = function() {
     changeImg11(), changeImg12(), changeImg13(), changeImg14(), changeImg15(), changeImg16(), changeImg17(), changeImg18(), changeImg19(), changeImg20()
 };
+
+//-------------Page Description: Pantalons Femme-----------------
+
+$(function(){
+	$(window).scroll(
+	function () {
+	if ($(this).scrollTop() > 580 && $(this).scrollTop()< 2700 ) { 
+	$('#description').addClass("fixdroite"); 
+	} else{
+    $('#description').removeClass("fixdroite");
+    }
+					}
+				);			 
+            });
+            
+
+
+
+
+
