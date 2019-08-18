4d492999244c2b31ec4e450c7c9731847a69f300
Maeva Masson <maeva.masson@gmail.com>
2019-08-18 13:39:57 +0200

    fix conflit

--- js/script2_f.js
+++ js/script2_f.js
@@ -178,19 +178,38 @@ window.onload = function() {
 
 //-------------Page Description: Pantalons Femme-----------------
 
-$(function(){
-	$(window).scroll(
-	function () {
-	if ($(this).scrollTop() > 580 && $(this).scrollTop()< 2700 ) { 
-	$('#description').addClass("fixdroite"); 
-	} else{
-    $('#description').removeClass("fixdroite");
-    }
-					}
-				);			 
-            });
-            
 
+// // When the user scrolls the page, execute myFunction 
+// window.onscroll = function() {myFunction()};
+
+// // Get the header
+// var descript_art = document.getElementById("description");
+
+// // Get the offset position of the navbar
+// var sticky = descript_art.offsetTop;
+
+// // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//   if (window.pageYOffset > sticky) {
+//     descript_art.classList.add("sticky");
+//   } else {
+//     descript_art.classList.remove("sticky");
+//   }
+// }
+
+// $(function(){
+// 	$(window).scroll(
+// 	function () {
+// 	if ($(this).scrollTop() > 520 && $(this).scrollTop()< 2200 ) { 
+// 	$('#description').addClass("fixdroite"); 
+// 	} else{
+//     $('#description').removeClass("fixdroite");
+//     }
+// 					}
+// 				);			 
+//             });
+            
+                        
 
 
 
